d14e2d126f97aae66a73e399b5f69a792c307ae1
hexa4ce <gabriel.dibiasi@proton.me>
2024-11-08 11:28:24 +0100

    test: Fix error comparison in FromFile test using errors.Is()

--- character_test.go
+++ character_test.go
@@ -1,6 +1,7 @@
 package character
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -21,7 +22,7 @@ func TestFromFile(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_, err := FromFile(tt.data)
-			if err != tt.wantErr {
+			if !errors.Is(err, tt.wantErr) {
 				t.Errorf("FromFile() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
